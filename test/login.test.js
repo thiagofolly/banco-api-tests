ab82b68227d62816b7b8b8180ef55add8fc7cf69
Thiago Folly <thiagofolly@gmail.com>
2026-06-21 21:21:26 -0300

    Iniciando o projeto

---
+++ test/login.test.js
@@ -0,0 +1,20 @@
+const request = require('supertest');
+const { expect } = require('chai');
+
+describe('Login', () => {
+    describe('POST /login', () => {
+        it('Deve retornar 200 com token em string quando usar credênciais válidas', async () => {
+            const response = await request('http://localhost:3000')
+                .post('/login')
+                .set('Content-Type', 'application/json')
+                .send({
+                    username: 'julio.lima',
+                    senha: '123456'
+                });
+
+            expect(response.status).to.equal(200);
+            expect(response.body.token).to.be.a('string');
+
+        })
+    })
+})
