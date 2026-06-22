6d33993540086fadbce9a20d444ebd6460c2c6fa
Thiago Folly <thiagofolly@gmail.com>
2026-06-21 22:44:01 -0300

    Adicionando fixtures para gerir os corpos das requisições

--- helpers/autenticacao.js
+++ helpers/autenticacao.js
@@ -1,13 +1,14 @@
 const request = require('supertest');
+const postLogin = require('../fixtures/postLogin.json')
 
 const obterToken = async (usuario, senha) => {
+    const bodyLogin = { ...postLogin }
+    bodyLogin.username = usuario;
+    bodyLogin.senha = senha;
     const loginResponse = await request(process.env.BASE_URL)
         .post('/login')
         .set('Content-Type', 'application/json')
-        .send({
-            'username': usuario,
-            'senha': senha
-        });
+        .send(bodyLogin);
 
     return loginResponse.body.token;
 }
